eb3427332ece737dede0be1be9ea80ba6c6a53b9
MitkovA <tonimitkov2s@gmail.com>
2018-02-06 12:43:49 +0000

    A-1:Base Android files

---
+++ app/src/main/java/com/cet325/gamers_emotional_state_detection/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.cet325.gamers_emotional_state_detection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
