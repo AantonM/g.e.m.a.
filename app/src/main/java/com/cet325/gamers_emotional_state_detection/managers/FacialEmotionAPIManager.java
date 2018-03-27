7c2e61f7013bdb1224bcec0c725aa276eb5fbd23
MitkovA <tonimitkov2s@gmail.com>
2018-03-27 23:39:56 +0100

    A-40: Added comments

--- app/src/main/java/com/cet325/gamers_emotional_state_detection/managers/FacialEmotionAPIManager.java
+++ app/src/main/java/com/cet325/gamers_emotional_state_detection/managers/FacialEmotionAPIManager.java
@@ -8,6 +8,10 @@ import com.cet325.gamers_emotional_state_detection.activities.GameplayActivity;
 import com.cet325.gamers_emotional_state_detection.handlers.EmotionRecognitionApiHandler;
 import com.cet325.gamers_emotional_state_detection.holders.ImageHolder;
 
+/**
+ * Manager that is responsible for the EmotionRecognitionApiHandler and
+ * the process of stating and stopping the calls to the FaceAPI
+ */
 public class FacialEmotionAPIManager {
 
     private ImageHolder imageHolder;
@@ -15,12 +19,22 @@ public class FacialEmotionAPIManager {
     private int imageNumber;
     private OnDataSendToGameplayActivity dataSendToActivity;
 
+    /**
+     * Contructor gets the imageHolder istance
+     * and initializes the data sender to the activity interface
+     *
+     * @param activity Activity
+     */
     public FacialEmotionAPIManager(Activity activity) {
         imageHolder = ImageHolder.getInstance();
         imageNumber = 0;
         dataSendToActivity = (GameplayActivity) activity;
     }
 
+    /**
+     * Method that starts the EmotionalFaceRecognition process
+     * if there is a new image available
+     */
     public void createEmotionRequiest() {
         if(thereIsANewImage())
         {
@@ -29,6 +43,12 @@ public class FacialEmotionAPIManager {
         }
     }
 
+    /**
+     * Method that checks if there is a new image
+     * stored in the holder
+     *
+     * @return boolean - true: if there is a new image, false:
+     */
     private boolean thereIsANewImage() {
 
         if(imageHolder.getImageNumber() == imageNumber) {
@@ -43,6 +63,9 @@ public class FacialEmotionAPIManager {
         return false;
     }
 
+    /**
+     * Method that stops the API to from the Handlers
+     */
     public void stopFacialRecognition()
     {
         Log.d("DevDebug", "EmotionRecognitionAPIManager: API recognition stopped.");
